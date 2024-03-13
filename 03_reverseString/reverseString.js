421510cb9d5ffd7ab46c2e2a353d92b4c66c6299
pbrumleve <paul.brumleve@gmail.com>
2024-03-13 18:48:18 -0500

    Created function to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let result
+  return result = Array.from(str).reverse().join('');
 };
 
 // Do not edit below this line
