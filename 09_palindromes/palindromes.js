9eed9bccb2b81ff213506f6ee5f513cc23f8b6e5
pbrumleve <paul.brumleve@gmail.com>
2024-03-14 02:33:07 -0500

    Created function to test for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let result;
+  let array = Array.from(str);
+  let onlyLetters = [];
+  for (const letter of array) {
+    if (isLetter(letter) === null) {
+    } else {
+      onlyLetters += letter;
+    };
+  }
+  let reverseArray = Array.from(onlyLetters).reverse().join('');
+  if (onlyLetters.toLowerCase() === reverseArray.toLowerCase()) {
+    result = true;
+  } else {result = false};
+  return result
 };
+
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
