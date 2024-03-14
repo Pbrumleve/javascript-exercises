24801805e12c74cacd241793e3661e382d8656f9
pbrumleve <paul.brumleve@gmail.com>
2024-03-13 23:36:03 -0500

    Created function that checks and removes elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function fun(array, ...theArgs) {
+  let newArray
+  for (let i = 0; i < theArgs.length; i++) {
+    let index = array.indexOf(theArgs[i]);
+    if (index > -1) {
+      array.splice(index, 1);
+      newArray = array;
+    };
+  };
+  return array;
 };
 
 // Do not edit below this line
