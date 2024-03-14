f937464a72ab60cafe8f54999057be250f664d8d
pbrumleve <paul.brumleve@gmail.com>
2024-03-14 01:02:27 -0500

    Created function to convert between C and F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let absNum = (temp - 32) * 5 / 9;
+  return round(absNum);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let absNum = temp * 9 / 5 + 32
+  return round(absNum);
+};
 
+function round(num) {
+  let stepOne = 10 * num;
+  return Math.round(stepOne) / 10;
 };
 
 // Do not edit below this line
