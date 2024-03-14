6674ce51df00382cc482177b66b0cb2e05073a14
pbrumleve <paul.brumleve@gmail.com>
2024-03-14 00:01:26 -0500

    Created function to add all integers between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let sum = 0;
+  if (a >= 0 && b >= 0 && typeof a === 'number' && typeof b === 'number') {
+    if (0 <= a < b) {
+      for (let i = a; i <= b; i++) {
+        sum += i;
+      };
+    } else if (0 <= b < a) {
+      for (let i = b; i <= a; i++) {
+        sum += i;
+      } 
+    } else {
+        sum = 'ERROR';
+      };
+      return sum;
+  } else {return sum = 'ERROR'};
 };
 
 // Do not edit below this line
