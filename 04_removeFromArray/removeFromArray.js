dbbdb1c2c51b14fd8ff8799a7de31b73d28c608f
pbrumleve <paul.brumleve@gmail.com>
2024-03-13 23:37:50 -0500

    Cleaned up code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,8 @@
-const removeFromArray = function fun(array, ...theArgs) {
-  let newArray
+const removeFromArray = function(array, ...theArgs) {
   for (let i = 0; i < theArgs.length; i++) {
     let index = array.indexOf(theArgs[i]);
     if (index > -1) {
       array.splice(index, 1);
-      newArray = array;
     };
   };
   return array;
