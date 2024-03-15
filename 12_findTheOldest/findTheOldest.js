dcc580334d01c5f4428625b57867c4e5b9a7da4c
pbrumleve <paul.brumleve@gmail.com>
2024-03-15 14:34:20 -0500

    Created function to find oldest in an object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(obj) {
+  let death = new Date().getFullYear();
+  const sortOldest = obj.sort(function(a, b) {
+    if (!a.yearOfDeath) {
+    const lastInventor = death - a.yearOfBirth;
+    const nextInventor = b.yearOfDeath - b.yearOfBirth;
+    return lastInventor > nextInventor ? -1 : 1;
+    } else if(!b.yearOfDeath) {
+    const lastInventor = a.yearOfDeath - a.yearOfBirth;
+    const nextInventor = death - b.yearOfBirth;
+    return lastInventor > nextInventor ? -1 : 1;
+    } else {
+    const lastInventor = a.yearOfDeath - a.yearOfBirth;
+    const nextInventor = b.yearOfDeath - b.yearOfBirth;
+    return lastInventor > nextInventor ? -1 : 1;
+    }
+    })
+  console.log(sortOldest);
+  let oldest = sortOldest[0];
+  console.log(oldest);
+  return oldest;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
