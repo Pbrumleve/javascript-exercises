6dfbad773d943d5d2c6a4f01f8e9ad8b6ce05c54
pbrumleve <paul.brumleve@gmail.com>
2024-03-15 00:24:40 -0500

    Created fibonnaci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+  if (x < 0) {
+    return 'OOPS';
+  } else {
+    let fib = 1;
+    let currentFib = 0;
+    let lastFib = 0;
+    for (let i = 0; i < x; i++) {
+      lastFib = currentFib;
+      currentFib = fib;
+      fib += lastFib;
+    };
+    return currentFib;
+  };
 };
 
 // Do not edit below this line
